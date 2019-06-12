05f936edece7f90fe2a572e2032926109a76d3f2
Mark Dabler <mdabler@rightpoint.com>
2019-06-11 19:15:17 -0500

    Added solutions for module: 04 lesson: 1

--- Scripts/git_student_push_solutions.py
+++ Scripts/git_student_push_solutions.py
@@ -9,28 +9,73 @@ import shlex
 import sys
 from git import Repo
 import os
+import shutil
+
+def copy_and_check(src, dst):
+    if os.path.isdir(src):
+        # Get directory contents from src path
+        shutil.copytree(src, dst)
+
+        # Check if that directory exists in dst path
+        # If not, create directory with same name
+        # Copy
+
+        
+        #directory_index = src.rfind("/")
+        # directory = src[directory_index:]
+        # print("end of src: " + directory)
 
 repo_path = os.path.join(os.getcwd(), "../")
 file_path = os.path.join(os.getcwd(), "test.txt")
 
+command_line = sys.argv
+module_num = command_line[1]
+lesson_num = command_line[2]
+commit_message = f"Added solutions for module: {module_num} lesson: {lesson_num}"
+
+# Standard paths for teacher and student modules
+modules_path_teacher = os.path.join("..", "..", "DataViz-Lesson-Plans", "01-Lesson-Plans")
+#################### FOR TESTING ##############################
+modules_path_student = os.path.join("..", "..", "git-auto-northwestern-data-science", "Scripts", "Activities")
+# modules_path_student = os.path.join("..", "..", "NUCHI201905DATA2")
+
+# Get directory that corresponds to entered module_num
+directories = os.listdir(modules_path_teacher)
+directory = [f for f in directories if str(module_num) in f]
+directory_path = os.path.join(directory[0], str(lesson_num), "Activities")
+
+# Append directory_path to the correct module_num to the src and dst paths
+src_path_teacher = os.path.join(modules_path_teacher, directory_path)
+#################### FOR TESTING ##############################
+dst_path_student = modules_path_student
+# dst_path_student = os.path.join(modules_path_student, directory_path)
+
+# Get all file names from src and dst file paths
+src_all_files_teacher = os.listdir(src_path_teacher)
+dst_all_files_student = os.listdir(dst_path_student)
+
+for f in src_all_files_teacher:
+    # This means it a student directory
+    if "Stu" in f:
+        # If not created, create the directory in destination
+        if f not in dst_all_files_student:
+            new_directory = os.path.join(dst_path_student, f)
+            print("NEW: " + new_directory)
+            os.mkdir(new_directory)
+        src_path = os.path.join(src_path_teacher, f, "Solved")
+        dst_path = os.path.join(dst_path_student, f, "Solved")
+        copy_and_check(src_path, dst_path)
+        
 repo = Repo(repo_path)
+assert not repo.bare
 master = repo.heads.master
-print(repo.git.add("."))
-print(repo.git.commit(m="test message"))
-# print(repo.git.status())
-# print(repo.git.push())
-cmd = ['git', 'push']
-p = Popen(cmd)
-p.wait()
-print("Successfuly pushed from script!")
 
-
-# print(repo)
-# assert not repo.bare
-# command_line = sys.argv
-# module_num = command_line[1]
-# lesson_num = command_line[2]
-
-# p = Popen(["git", "pull"], stdout=PIPE)
-# stuff = p.communicate()
-# print(stuff)
+try:
+    repo.git.add(".")
+    repo.git.commit(m = commit_message)
+    cmd = ['git', 'push']
+    p = Popen(cmd)
+    p.wait()
+    print("Successfuly pushed from script!")
+except:
+    print("Error occured. Git script ran unsuccessfully!")
